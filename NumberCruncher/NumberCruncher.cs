eb1555904ea7b24c8f7ac57cdd2de915e1e6f33f
ccartagena879 <ccartagena879@gmail.com>
2019-03-31 12:12:01 -0500

    test not working

--- NumberCruncher/NumberCruncher.cs
+++ NumberCruncher/NumberCruncher.cs
@@ -1,29 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace NumberCruncher
 {
-    public static class Program
+    public static class NumberUtility
     {
-        public static void Main()
+        public class NumberCruncher
         {
-            List<int> NumberList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            Console.WriteLine(NumberUtility.NumberCruncher.SumOfEvenNumbers(NumberList));
-            Console.ReadLine();
-        }
-
-
-   /* public class NumberCruncher
-        {
-
-           
             public static int SumOfEvenNumbers(List<int> NumberList)
             {
                 //method should find the sum of all even numbers in NumberList
                 var result = NumberList.Where(i => i % 2 == 0).Sum();
                 return result;
             }
-            */
-            
+        }
     }
 }
